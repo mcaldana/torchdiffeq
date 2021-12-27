377cbfae3eaf0a754382e1ccd4737e429899d577
Matteo Caldana <matteo1.caldana@mail.polimi.it>
2021-12-27 10:24:03 +0100

    initial tests

--- ode_demo.py
+++ ode_demo.py
@@ -6,25 +6,30 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchdiffeq import odeint_adjoint as odeint
+
+import logging
+import datetime
 
 parser = argparse.ArgumentParser('ODE demo')
-parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
 parser.add_argument('--data_size', type=int, default=1000)
 parser.add_argument('--batch_time', type=int, default=10)
 parser.add_argument('--batch_size', type=int, default=20)
 parser.add_argument('--niters', type=int, default=2000)
 parser.add_argument('--test_freq', type=int, default=20)
+parser.add_argument('--seed', type=int, default=0)
 parser.add_argument('--viz', action='store_true')
-parser.add_argument('--gpu', type=int, default=0)
-parser.add_argument('--adjoint', action='store_true')
 args = parser.parse_args()
 
-if args.adjoint:
-    from torchdiffeq import odeint_adjoint as odeint
-else:
-    from torchdiffeq import odeint
+logging.basicConfig(filename='./logs/out{}.log'.format(int(datetime.datetime.now().timestamp())), level=logging.DEBUG)
+logging.info('data_size:{}, batch_time:{}, batch_size:{}, test_freq:{}, seed:{}'.format(
+                args.data_size,  args.batch_time, args.batch_size, args.test_freq, args.seed)
+            )
 
-device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+device = torch.device('cpu')
 
 true_y0 = torch.tensor([[2., 0.]]).to(device)
 t = torch.linspace(0., 25., args.data_size).to(device)
@@ -170,12 +175,13 @@ if __name__ == '__main__':
 
         time_meter.update(time.time() - end)
         loss_meter.update(loss.item())
+        logging.info(loss.item())
 
         if itr % args.test_freq == 0:
             with torch.no_grad():
                 pred_y = odeint(func, true_y0, t)
                 loss = torch.mean(torch.abs(pred_y - true_y))
-                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                logging.info('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
                 visualize(true_y, pred_y, func, ii)
                 ii += 1
 
